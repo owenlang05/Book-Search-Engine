ed3374cb7074dd440182b68d935107aaf3345e10
Owen Lang <owen.lang146@gmail.com>
2023-11-14 01:04:04 -0600

    Apollo Provider and useMutation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,47 @@
-import './App.css';
 import { Outlet } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
 
-import Navbar from './components/Navbar';
+import { StoreProvider} from './utils/store-context';
+import Nav from './components/Nav';
+
+import Auth from './utils/auth'
+
+import './App.css'
+
+const httpLink = createHttpLink({ uri: '/graphql'});
+
+const authLink = setContext((_, {headers}) => {
+  const token = Auth.getToken();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+});
 
 function App() {
+
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
-  );
+    <ApolloProvider client={client}>
+      <StoreProvider>
+        <Nav />
+        <Outlet />
+      </StoreProvider>
+    </ApolloProvider>
+  )
 }
 
-export default App;
+export default App
--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -9,18 +9,21 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
+import { SAVE_BOOK } from '../utils/mutations';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
   // create state for holding our search field data
   const [searchInput, setSearchInput] = useState('');
-
+  
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
-
+  
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
@@ -72,7 +75,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({variables: {input: bookToSave}});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
